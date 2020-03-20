50d97399353d8b3f2ca2d94285ae3f29f942e4cd
Slava <solovey.1999@mail.ru>
2020-03-21 01:49:59 +0700

    5.9 В класс ArgHandler вносим методы проверки наличия аргументов (п 4.1), вывода справки (п 4.2), нужна ли аутентификация(п 5.1)

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -1,7 +1,35 @@
+import kotlin.system.exitProcess
+
 class ArgHandler(args:Array<String>) {
     val login:String = args[1]
     val password:String = args[3]
+    fun ChekArg(args: Array<String>):Int
+    {
+        if(args.isEmpty())
+            exitProcess(0)
+        else
+            exitProcess(1)
+    }
+    fun PrintHelp()
+    {
+        print(Help)
+    }
+    val Help:String = "-h - вызов справки\n" +
+            "        -login <str> -pass <str> - аутентификация s\n" +
+            "        -login <str> -pass <str> -res <str> -role <str> - авторизация к введенному ресурсу\n" +
+            "        -ds <YYYY-MM-DD> -de <YYYY-MM-DD> -vol <int> - занесение данных об использовании ресурса(только после авторизации"
+    fun ChekHelp(args: Array<String>)
+    {
+        if(args[0]=="-h")
+            PrintHelp()
+        else
+            exitProcess(0)
+    }
+    fun NeedAuth(args: Array<String>) {
+        if (args[0] == "-login" && args[2] == "-pass")
+            exitProcess(0)
 
+    }
 
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,33 +2,6 @@ import kotlin.system.exitProcess
 
 fun main(args:Array<String>)
 {
-    ChekArg(args)
-}
-fun ChekArg(args: Array<String>):Int
-{
-    if(args.isEmpty())
-        exitProcess(0)
-    else
-        exitProcess(1)
-}
-fun PrintHelp()
-{
-    print(Help)
-}
-const val Help:String = "-h - вызов справки\n" +
-        "        -login <str> -pass <str> - аутентификация s\n" +
-        "        -login <str> -pass <str> -res <str> -role <str> - авторизация к введенному ресурсу\n" +
-        "        -ds <YYYY-MM-DD> -de <YYYY-MM-DD> -vol <int> - занесение данных об использовании ресурса(только после авторизации"
-fun ChekHelp(args: Array<String>)
-{
-    if(args[0]=="-h")
-        PrintHelp()
-    else
-        exitProcess(0)
-}
-fun NeedAuth(args: Array<String>) {
-    if (args[0] == "-login" && args[2] == "-pass")
-        exitProcess(0)
 
 }
 fun ValidateLogin(args: Array<String>){
