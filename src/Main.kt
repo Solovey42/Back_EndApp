9a2852f500155db16ff6e197b84835d9be3fd23b
Slava <solovey.1999@mail.ru>
2020-03-20 22:12:12 +0700

    4.3 Проверяем надо ли выводить справку

--- src/Main.kt
+++ src/Main.kt
@@ -19,3 +19,10 @@ const val Help:String = "-h - вызов справки\n" +
         "        -login <str> -pass <str> - аутентификация s\n" +
         "        -login <str> -pass <str> -res <str> -role <str> - авторизация к введенному ресурсу\n" +
         "        -ds <YYYY-MM-DD> -de <YYYY-MM-DD> -vol <int> - занесение данных об использовании ресурса(только после авторизации"
+fun ChekHelp(args: Array<String>)
+{
+    if(args[0]=="-h")
+        PrintHelp()
+    else
+        exitProcess(0)
+}
