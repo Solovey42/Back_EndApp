6ebd97a0255d241d24edff3cbb8805017a69261c
Slava <solovey.1999@mail.ru>
2020-03-22 20:00:12 +0700

    6.9 Модифицируем класс ArgHandler на работу с параметрами res и role

--- src/ArgHandler.kt
+++ src/ArgHandler.kt
@@ -4,6 +4,8 @@ class ArgHandler(args: Array<String>) {
     private val arr: Array<String> = args
     val login: String = args[1]
     val password: String = args[3]
+    val res: String = args [5]
+    val role: String = args [7]
 
 
     fun ChekArg(): Int {
