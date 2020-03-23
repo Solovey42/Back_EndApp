6a35fcae7e54ce8b28d90fdfe6ebed1790f4429d
Slava <solovey.1999@mail.ru>
2020-03-23 14:42:55 +0700

    Коллекции перенесены в main

--- src/Collection.kt
+++
@@ -1,3 +0,0 @@
-var Users: List<User> = listOf(User("vasya", "123"), User("admin", "admin"), User("q", "?!@"), User("abcd", "qwerty"))
-var Resources: List<Resource> = listOf(Resource("A", Roles.READ, Users[0]), Resource("A.B.C", Roles.WRITE, Users[0]), Resource("A.B", Roles.EXECUTE, Users[1]), Resource("A", Roles.READ, Users[1]), Resource("A.B", Roles.WRITE, Users[1]), Resource("A.B.C", Roles.READ, Users[1]), Resource("B", Roles.EXECUTE, Users[2]), Resource("A.A.A", Roles.EXECUTE, Users[0]))
-var Sessions: MutableList<Session> = mutableListOf()
