1d0cd8a1b82f88bf806b9d97a3bc38bee36f7b80
Slava <solovey.1999@mail.ru>
2020-03-23 14:58:26 +0700

    CodeStyle

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,3 @@
-var Users: List<User> = listOf(User("vasya", "123"), User("admin", "admin"), User("q", "?!@"), User("abcd", "qwerty"))
-var Resources: List<Resource> = listOf(Resource("A", Roles.READ, Users[0]), Resource("A.B.C", Roles.WRITE, Users[0]), Resource("A.B", Roles.EXECUTE, Users[1]), Resource("A", Roles.READ, Users[1]), Resource("A.B", Roles.WRITE, Users[1]), Resource("A.B.C", Roles.READ, Users[1]), Resource("B", Roles.EXECUTE, Users[2]), Resource("A.A.A", Roles.EXECUTE, Users[0]))
-var Sessions: MutableList<Session> = mutableListOf()
-
 fun main(args: Array<String>) {
 
     val argHandler = ArgHandler(args)
@@ -10,8 +6,13 @@ fun main(args: Array<String>) {
     Authentication(argHandler)
 
 
-
 }
+
+var Users: List<User> = listOf(User("vasya", "123"), User("admin", "admin"), User("q", "?!@"), User("abcd", "qwerty"))
+
+var Resources: List<Resource> = listOf(Resource("A", Roles.READ, Users[0]), Resource("A.B.C", Roles.WRITE, Users[0]), Resource("A.B", Roles.EXECUTE, Users[1]), Resource("A", Roles.READ, Users[1]), Resource("A.B", Roles.WRITE, Users[1]), Resource("A.B.C", Roles.READ, Users[1]), Resource("B", Roles.EXECUTE, Users[2]), Resource("A.A.A", Roles.EXECUTE, Users[0]))
+
+var Sessions: MutableList<Session> = mutableListOf()
 
 
 
