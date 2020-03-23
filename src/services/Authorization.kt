8b4150fb979e6fc2d67c93db5e9bc9f2b78b390b
Slava <solovey.1999@mail.ru>
2020-03-24 01:17:33 +0700

    Созданы поля arg и user, проведен рефакторинг для их использования

--- src/services/Authorization.kt
+++ src/services/Authorization.kt
@@ -4,40 +4,46 @@ import ArgHandler
 import enums.ExitCodes
 import Resources
 import enums.Roles
+import models.Resource
 import models.User
 import kotlin.system.exitProcess
 
-class Authorization(argHandler: ArgHandler) {
+class Authorization(argHandler: ArgHandler, User: User) {
+
+    private val user = User
+    private val arg = argHandler
 
 
     init {
         if (argHandler.NeedAuthorization())
-            start(argHandler)
+            start()
     }
 
-    private fun start(argHandler: ArgHandler) {
-        if (argHandler.NeedAuthorization())
-            checkResRole(argHandler)
+    private fun start() {
+        if (arg.NeedAuthorization())
+            checkResRole()
 
     }
 
-    private fun checkResRole(argHandler: ArgHandler){
-        if (argHandler.role != Roles.EXECUTE.name && argHandler.role != Roles.READ.name && argHandler.role != Roles.WRITE.name)
+    private fun checkResRole() {
+        if (arg.role != Roles.EXECUTE.name && arg.role != Roles.READ.name && arg.role != Roles.WRITE.name)
             exitProcess(ExitCodes.UnknownRole.code)
-        if (!argHandler.CheckResName())
+        if (!arg.CheckResName())
             exitProcess((ExitCodes.UnknownRole.code))
 
-        val nodes = argHandler.res.split(".")
+        val nodes = arg.res.split(".")
         for (index in nodes.indices) {
             val currentNode = nodes.subList(0, index + 1).joinToString(".")
-            if (Resources.any { it.res == currentNode && it.role.name == argHandler.role })
-                if (!argHandler.NeedAcc())
+            if (Resources.any { it.res == currentNode && it.role.name == arg.role && it.user == user })
+                if (!arg.NeedAcc())
                     exitProcess(ExitCodes.Success.code)
                 else
-                    Accounting(argHandler)
+                    Accounting(arg, user)
 
         }
 
         exitProcess(ExitCodes.NoAccess.code)
     }
+
+
 }
