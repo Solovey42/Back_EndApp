506d59d6be1f6269c9851358cf350ccece0719b5
Slava <solovey.1999@gmail.com>
2020-04-02 20:46:25 +0700

    Исправлены ошибки

--- RUN.sh
+++ RUN.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "msys" ]]; then
-  params="bin/app.jar"
-else
-  params="bin/app.jar"
-fi
-
-# shellcheck disable=SC2068
-java -classpath $params com.solo.myProject.MainKt $@
+java -jar ./bin/app.jar
