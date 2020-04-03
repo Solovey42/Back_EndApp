e70f326ba559a42c947dc77174e2f74a99a8d95d
Slava <solovey.1999@gmail.com>
2020-04-04 04:29:44 +0700

    Добавлена проверка на os

--- BUILD.sh
+++ BUILD.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "msys" ]]; then
+  params="./lib/log4j-api-2.13.1.jar;./lib/log4j-core-2.13.1.jar"
+else
+  params="./lib/log4j-api-2.13.1.jar:./lib/log4j-core-2.13.1.jar"
+fi
+
+
 mkdir -p bin
-kotlinc -include-runtime -d bin/app.jar src/ -cp "\
-lib/log4j-api-2.13.1.jar;\
-lib/log4j-core-2.13.1.jar"
+kotlinc -include-runtime -d bin/app.jar src/ -cp $params
