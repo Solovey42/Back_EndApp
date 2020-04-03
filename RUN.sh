9ac88ab34e23abd5b8b2faf906994da0f7285356
Slava <solovey.1999@gmail.com>
2020-04-04 04:10:04 +0700

    Исправлена опечатка в пути

--- RUN.sh
+++ RUN.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "msys" ]]; then
-  params="./lib/log4j-api-2.13.1.jar;./lib/log4j-core-2.13.1.jar;bin/app.jar"
+  params="./lib/log4j-api-2.13.1.jar;./lib/log4j-core-2.13.1.jar;./bin/app.jar"
 else
-  params="./lib/log4j-api-2.13.1.jar:./lib/log4j-core-2.13.1.jar:bin/app.jar"
+  params="./lib/log4j-api-2.13.1.jar:./lib/log4j-core-2.13.1.jar:./bin/app.jar"
 fi
 
 # shellcheck disable=SC2068
