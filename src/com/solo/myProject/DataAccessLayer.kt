830d940e9c0559503eea5a23e0728b4af348a90c
Slava <solovey.1999@gmail.com>
2020-04-10 16:18:41 +0700

    Добавлен метод проверяющий наличие данного логина в бд

--- src/com/solo/myProject/DataAccessLayer.kt
+++ src/com/solo/myProject/DataAccessLayer.kt
@@ -30,7 +30,7 @@ class DataAccessLayer(private val conn: Connection, private val users: MutableLi
 
     fun getUser(login: String): User {
         logger.info("Get prepared statement with users")
-        val getUser = conn!!.prepareStatement("SELECT hash, salt FROM users WHERE login = ?")
+        val getUser = conn.prepareStatement("SELECT hash, salt FROM users WHERE login = ?")
         getUser.setString(1, login)
         logger.info("Get result set with user")
         val res = getUser.executeQuery()
@@ -54,5 +54,20 @@ class DataAccessLayer(private val conn: Connection, private val users: MutableLi
         statement.close()
         resultSet.close()
         logger.info("CreateStatement close")
+    }
+
+    fun loginExists(login: String): Boolean {
+        logger.info("Get prepared statement with user")
+        val getUser = conn.prepareStatement("SELECT count(*) FROM users WHERE login = ?")
+        getUser.setString(1, login)
+        logger.info("Get result set with user")
+        val res = getUser.executeQuery()
+        res.next()
+        val ans = res.getInt(1) > 0
+        logger.info("Close result set with user")
+        res.close()
+        logger.info("Close prepared statement with user")
+        getUser.close()
+        return ans
     }
 }
