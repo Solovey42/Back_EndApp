5b8aaeb627c8e071c03650435de0a083e2f7a5ac
Slava <solovey.1999@gmail.com>
2020-04-10 21:24:56 +0700

    Отрефакторена модель сессии

--- src/com/solo/myProject/models/Session.kt
+++ src/com/solo/myProject/models/Session.kt
@@ -2,4 +2,4 @@ package com.solo.myProject.models
 
 import java.time.LocalDate
 
-data class Session(val user: User, val res: Resource, val ds: LocalDate, val de: LocalDate, val vol: Int)
+data class Session(val user: User, val res: String, val ds: LocalDate, val de: LocalDate, val vol: Int)
